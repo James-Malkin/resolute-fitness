dfecf986d329bf6a69e7556dc98c0ae936d62e5b
James Malkin <j.steven.malkin@gmail.com>
2025-03-09 23:09:46 +0000

    add navbar controller to handle mobile navigation drawer

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "menu" ]
+
+  mql = window.matchMedia("(min-width: 768px)")
+
+
+  connect() {
+    this.mql.addEventListener("change", (e) => {e.matches && this.close()})
+  }
+
+  toggle(event) {
+    this.element.classList.toggle("open")
+
+    document.body.classList.toggle("overflow-hidden", this.element.classList.contains("open"))
+  
+    const button = event.currentTarget
+    button.setAttribute("aria-expanded", this.element.classList.contains("open"))
+  }
+
+  close() {
+    this.element.classList.remove("open")
+    document.body.classList.remove("overflow-hidden")
+    this.menuTarget.setAttribute("aria-expanded", false)
+  }
+}
