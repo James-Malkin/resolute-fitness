cdbd59885f355b650b4ceaed9a2fd594a9da241e
James Malkin <j.steven.malkin@gmail.com>
2025-03-14 23:53:24 +0000

    make home send email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    TestMailer.test_email.deliver_now
+  end
 end
--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,4 +1,5 @@
 class TestMailer < ApplicationMailer
   def test_email
     mail(to: 'j.steven.malkin@gmail.com', subject: 'Test Email')
+  end
 end
