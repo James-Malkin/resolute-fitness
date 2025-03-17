a06ec08bd3c2a7a2deb05830218f4a9c4f9b43af
James Malkin <j.steven.malkin@gmail.com>
2025-03-17 17:07:20 +0000

    add confirmation email to signup

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_250_217_201_729) do
+ActiveRecord::Schema[8.0].define(version: 20_250_317_164_428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_catalog.plpgsql'
 
@@ -25,6 +25,13 @@ ActiveRecord::Schema[8.0].define(version: 20_250_217_201_729) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'username', null: false
+    t.string 'provider'
+    t.string 'uid'
+    t.string 'confirmation_token'
+    t.datetime 'confirmed_at'
+    t.datetime 'confirmation_sent_at'
+    t.string 'unconfirmed_email'
+    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
     t.index ['username'], name: 'index_users_on_username', unique: true
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -5,5 +5,8 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     username { Faker::Internet.username }
+    confirmed_at { Time.current }
+    confirmation_sent_at { Time.current }
+    confirmation_token { Devise.token_generator.generate(User, :confirmation_token).first }
   end
 end
