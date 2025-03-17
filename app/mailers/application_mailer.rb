51f4af0179095d28afc184ae7a90c795ae40b7f7
James Malkin <j.steven.malkin@gmail.com>
2025-03-17 20:08:56 +0000

    fix bootstrap mailer for devise

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,9 +3,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'no-reply@resolutefitness.studio'
   layout 'bootstrap-mailer'
-
-  def devise_mail(record, action, opts = {}, &)
-    initialize_from_record(record)
-    bootstrap_mail(headers_for(action, opts), &)
-  end
 end
--- app/mailers/devise_bootstrap_mailer.rb
+++ app/mailers/devise_bootstrap_mailer.rb
@@ -2,4 +2,10 @@
 
 class DeviseBootstrapMailer < Devise::Mailer
   layout 'bootstrap-mailer'
+  default template_path: 'devise/mailer'
+
+  def devise_mail(record, action, opts = {}, &)
+    initialize_from_record(record)
+    bootstrap_mail(headers_for(action, opts), &)
+  end
 end
