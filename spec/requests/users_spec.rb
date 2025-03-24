3b8c16e428301cd1a78dd7122ac57dff4b80c640
James Malkin <j.steven.malkin@gmail.com>
2025-03-24 21:10:50 +0000

    add missed coverage for user requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,28 @@ require 'rails_helper'
 
 describe 'Users' do
   describe 'PATCH /users/:id' do
+    subject (:patch_user) { patch user_path(user), params: { user: { username: 'new_username' } } }
+
     let(:user) { create(:user) }
 
+    context 'when the update is succeeds' do
+      it 'updates the user' do
+        patch_user
+        user.reload
+        expect(user.username).to eq('new_username')
+      end
+    end
+
+    context 'when the update fails' do
+      before do
+        allow_any_instance_of(User).to receive(:update).and_return(false)
+      end
+
+      it 'does not update the user' do
+        patch_user
+        user.reload
+        expect(user.username).not_to eq('new_username')
+      end
+    end
   end
 end
