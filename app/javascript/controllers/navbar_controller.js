74ba39c8c8b5a3dd8721a180d8ac3558aa4dc834
James Malkin <j.steven.malkin@gmail.com>
2025-03-31 19:37:49 +0100

    seperate site and profile navigation

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,27 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "menu" ]
+  static targets = [ "mobileMenu", "profileMenu" ]
 
   mql = window.matchMedia("(min-width: 768px)")
 
 
   connect() {
-  this.mql.addEventListener("change", (e) => {e.matches && this.close()})
+    this.mql.addEventListener("change", (e) => {e.matches && this.close()})
+
+    this.element.addEventListener("click", (event) => {
+      if (event.target === this.element && this.element.classList.contains("open")) {
+        this.close()
+      }
+    })
   }
 
-  toggle(event) {
-    this.element.classList.toggle("open")
+  open(event) {
+    const targetMenu = event.currentTarget.dataset.target
 
-    document.body.classList.toggle("overflow-hidden", this.element.classList.contains("open"))
-  
-    const button = event.currentTarget
-    button.setAttribute("aria-expanded", this.element.classList.contains("open"))
+    this.element.classList.add("open")
+    this.element.dataset.target = targetMenu
+
+    document.body.classList.toggle("overflow-hidden", true)
+
+    this.targets.find(targetMenu).setAttribute("aria-expanded", true)
   }
 
   close() {
     this.element.classList.remove("open")
+
     document.body.classList.remove("overflow-hidden")
-    this.menuTarget.setAttribute("aria-expanded", false)
+
+    this.mobileMenuTarget.setAttribute("aria-expanded", false)
+    this.profileMenuTarget.setAttribute("aria-expanded", this.profileMenuTarget.classList.contains("open"))
   }
 }
