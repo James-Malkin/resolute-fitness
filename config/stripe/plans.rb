10e1d25701c2374bcd3722f9f5cf832b5ef4a6ff
James Malkin <j.steven.malkin@gmail.com>
2025-04-01 20:11:05 +0100

    install stripe gems

---
+++ config/stripe/plans.rb
@@ -0,0 +1,53 @@
+# This file contains descriptions of all your stripe plans
+
+# Example
+# Stripe::Plans::PRIMO #=> 'primo'
+
+# Stripe.plan :primo do |plan|
+#   # plan name as it will appear on credit card statements
+#   plan.name = 'Acme as a service PRIMO'
+#
+#   # amount in cents. This is 6.99
+#   plan.amount = 699
+#
+#   # currency to use for the plan (default 'usd')
+#   plan.currency = 'usd'
+#
+#   # interval must be either 'day', 'week', 'month' or 'year'
+#   plan.interval = 'month'
+#
+#   # only bill once every three months (default 1)
+#   plan.interval_count = 3
+#
+#   # number of days before charging customer's card (default 0)
+#   plan.trial_period_days = 30
+# end
+
+# Once you have your plans defined, you can run
+#
+#   rake stripe:prepare
+#
+# This will export any new plans to stripe.com so that you can
+# begin using them in your API calls.
+
+Stripe.plan :bronze do |plan|
+  plan.name = 'Bronze'
+  plan.amount = 1999
+  plan.currency = 'gbp'
+  plan.interval = 'month'
+end
+
+Stripe.plan :silver do |plan|
+  plan.name = 'Silver'
+  plan.amount = 2999
+  plan.currency = 'gbp'
+  plan.interval = 'month'
+end
+
+Stripe.plan :gold do |plan|
+  plan.name = 'Gold'
+  plan.amount = 3999
+  plan.currency = 'gbp'
+  plan.interval = 'month'
+end
+
