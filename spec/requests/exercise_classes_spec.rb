b51dabc1d3ceb7972e82a3dfb0110961826b7892
James Malkin <j.steven.malkin@gmail.com>
2025-04-07 14:43:55 +0100

    add new class creation

--- spec/requests/exercise_classes_spec.rb
+++ spec/requests/exercise_classes_spec.rb
@@ -20,4 +20,46 @@ describe 'Exercise Classes' do
       expect(response.status).to eq(200)
     end
   end
+
+  describe 'GET /classes/new' do
+    before do
+      get new_exercise_class_path
+    end
+
+    it 'returns a 200 status code' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the new class form' do
+      expect(response.body).to include('New Class')
+    end
+  end
+
+  describe 'POST /classes' do
+    subject(:post_classes) { post exercise_classes_path, params: { exercise_class: exercise_class_params } }
+
+    let!(:exercise_class_params) do
+      {
+        name: 'Yoga',
+        description: 'A relaxing yoga class.',
+        image: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'test_image.jpg'), 'image/jpeg')
+      }
+    end
+
+    context 'when the class is created successfully' do
+      it 'creates a new class' do
+        expect { post_classes }.to change(ExerciseClass, :count).by(1)
+      end
+    end
+
+    context 'when the class creation fails' do
+      before do
+        allow_any_instance_of(ExerciseClass).to receive(:save).and_return(false)
+      end
+
+      it 'does not create a new class' do
+        expect { post_classes }.not_to change(ExerciseClass, :count)
+      end
+    end
+  end
 end
