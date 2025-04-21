258358f2ffb79104f0481a2714a172361d2f3022
James Malkin <j.steven.malkin@gmail.com>
2025-04-21 09:59:06 +0100

    add routes

--- spec/requests/bookings_spec.rb
+++ spec/requests/bookings_spec.rb
@@ -18,4 +18,20 @@ describe 'Bookings' do
       expect(response.body).to include('Book a Class')
     end
   end
+
+  describe 'GET /bookings/new' do
+    let!(:class_schedule) { create(:class_schedule) }
+
+    before do
+      get new_booking_path
+    end
+
+    it 'returns a 200 status code' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the new booking page' do
+      expect(response.body).to include('Book a Class')
+    end
+  end
 end
