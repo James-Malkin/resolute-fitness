2debfa4c54ae34647caf8bbfdd92bdde7a053a0f
James Malkin <j.steven.malkin@gmail.com>
2025-06-30 21:31:11 +0100

    up to date

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,8 +4,6 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials[:sentry_dsn]
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  # Add data like request headers and IP for users,
-  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
 
   config.traces_sample_rate = 0.5
